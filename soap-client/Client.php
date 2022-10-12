95fc14ae5a870658c4f7ac3e281cd61d7ca8bfcf
derdorrr <derifauzi27@gmail.com>
2022-10-12 07:10:37 +0700

    commit

---
+++ soap-client/Client.php
@@ -0,0 +1,71 @@
+<?php
+error_reporting(1); // error ditampilkan
+class Client
+{
+	private $options, $api;
+
+	// function yang pertama kali di-load saat class dipanggil
+	public function __construct($uri, $location)
+	{
+		$this->options = array('location' => $location, 'uri' => $uri);
+
+		// buat objek baru dari class SOAP Client
+		$this->api = new SoapClient(NULL, $this->options);
+
+		// hapus variable dari memory
+		unset($uri, $location);
+	}
+
+	// function untuk menghapus selain huruf dan angka
+	public function filter($data)
+	{
+		$data = preg_replace('/[^a-zA-Z0-9]/', '', $data);
+		return $data;
+		unset($data);
+	}
+
+	public function tampil_semua_data()
+	{
+		$data = $this->api->tampil_semua_data();
+		return $data;
+		unset($data);
+	}
+
+	public function tampil_data($id_barang)
+	{
+		$id_barang = $this->filter($id_barang);
+		$data = $this->api->tampil_data($id_barang);
+		return $data;
+		unset($id_barang, $data);
+	}
+
+	public function tambah_data($data)
+	{
+		$this->api->tambah_data($data);
+		unset($data);
+	}
+
+	public function ubah_data($data)
+	{
+		$this->api->ubah_data($data);
+		unset($data);
+	}
+
+	public function hapus_data($id_barang)
+	{
+		$this->api->hapus_data($id_barang);
+		unset($id_barang);
+	}
+
+	// function yang terakhir kali di-load saat class dipanggil
+	public function __destruct()
+	{	// hapus variable dari memory
+		unset($this->options, $this->api);
+	}
+}
+
+// uri dan location server
+$uri = 'http://192.168.55.74';
+$location = $uri . '/soap-server/server.php';
+// buat objek baru dari class Client
+$abc = new Client($uri, $location);
---
+++ soap-server/server.php
@@ -0,0 +1,17 @@
+<?php
+error_reporting(1); // error ditampilkan
+include "Database.php";
+
+$uri = 'http://192.168.55.74';
+
+// set uri server
+$options = array('uri' => $uri);
+
+// buat objek baru dari class Soap Server
+$server = new SoapServer(NULL, $options);
+
+// masukkan class Database ke objek SOAP Server
+$server->setClass('Database');
+
+// jalankan menggunakan SOAP requests handler
+$server->handle();
